8723cbd09175c013d6d632a7423c6a016ac08d44
narasimha <narasimha@codebrahma.com>
2018-09-09 12:03:00 +0530

    added buttons in reacievables form

--- src/components/Recievables/RecievablesForm.js
+++ src/components/Recievables/RecievablesForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TextInput, Text} from 'react-native';
+import { View, TextInput, Text, Button} from 'react-native';
 import { TextInputWithLabel } from '../FormComponents/TextInputWithLabel';
 import { DatePickerWithLabel } from '../FormComponents/DatePickerWithLabel';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -27,6 +27,36 @@ export default class RecievablesForm extends Component {
         <DatePickerWithLabel
           label={<Icon name="calendar-plus-o" size={30} />}
         />
+
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}>
+          <View
+            style={{
+              flex: 1
+            }}
+          >
+            <Button
+              onPress={() => {}}
+              title="Cancel"
+            />
+          </View>
+          <View
+            style={{
+              flex: 1
+            }}
+          >
+            <Button
+              onPress={() => {}}
+              color="#ff5c5c"
+              title="Add"
+            />
+          </View>
+        </View>
+
+
       </View>
     );
   }
