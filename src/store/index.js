72aa032b433d6462a1346dbc010f1f69a1959499
narasimha <narasimha@codebrahma.com>
2018-09-09 17:27:01 +0530

    added redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
-
+import rootSaga from '../components/sagas/index';
 import { rootReducer } from '../reducers/index';
 
-export const store = createStore(rootReducer, applyMiddleware(logger));
+const sagaMiddleware = createSagaMiddleware()
+
+export const store = createStore(
+                rootReducer,
+                applyMiddleware(logger, sagaMiddleware)
+              );
+
+sagaMiddleware.run(rootSaga);
